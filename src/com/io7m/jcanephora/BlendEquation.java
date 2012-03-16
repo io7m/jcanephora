2e3fbc2b370a8f639d8f27fe755cec2fd240ae24
Mark Raynsford <code@io7m.com>
2012-03-16 17:46:13 +0000

    Add blend functions

---
+++ src/com/io7m/jcanephora/BlendEquation.java
@@ -0,0 +1,14 @@
+package com.io7m.jcanephora;
+
+/**
+ * Blend equation specification.
+ */
+
+public enum BlendEquation
+{
+  BLEND_EQUATION_ADD,
+  BLEND_EQUATION_SUBTRACT,
+  BLEND_EQUATION_REVERSE_SUBTRACT,
+  BLEND_EQUATION_MINIMUM,
+  BLEND_EQUATION_MAXIMUM
+}
