401d1e901969e1617b65daf8b1b198f005b156e5
Mark Raynsford <code@io7m.com>
2012-03-17 19:06:09 +0000

    Simplify texture example

--- src/com/io7m/jcanephora/examples/lwjgl30/TextureExampleSimple.java
+++ src/com/io7m/jcanephora/examples/lwjgl30/TextureExampleSimple.java
@@ -1,6 +1,5 @@
 package com.io7m.jcanephora.examples.lwjgl30;
 
-import java.nio.ByteBuffer;
 import java.util.Properties;
 
 import javax.annotation.Nonnull;
@@ -12,6 +11,8 @@ import com.io7m.jcanephora.GLException;
 import com.io7m.jcanephora.GLInterface;
 import com.io7m.jcanephora.GLInterfaceLWJGL30;
 import com.io7m.jcanephora.LWJGL30;
+import com.io7m.jcanephora.PixelUnpackBufferCursorWritable4b;
+import com.io7m.jcanephora.PixelUnpackBufferWritableMap;
 import com.io7m.jcanephora.Texture2DRGBA;
 import com.io7m.jcanephora.TextureFilter;
 import com.io7m.jcanephora.TextureWrap;
@@ -72,32 +73,28 @@ public final class TextureExampleSimple
     /*
      * Textures are automatically backed by a pixel unpack buffer. This buffer
      * is mapped into the program's address space in order to be populated
-     * with bitmap data.
+     * with bitmap data. A cursor is obtained in order to safely insert four
+     * values of type 'byte' for each element.
      */
 
     {
-      final ByteBuffer map =
-        this.gl
-          .mapPixelUnpackBufferWrite(this.texture.getBuffer())
-          .getByteBuffer();
+      final PixelUnpackBufferWritableMap map =
+        this.gl.mapPixelUnpackBufferWrite(this.texture.getBuffer());
+      final PixelUnpackBufferCursorWritable4b c = map.getCursor4b();
 
-      assert (map.remaining() == (TextureExampleSimple.TEXTURE_WIDTH
-        * TextureExampleSimple.TEXTURE_HEIGHT * 4));
-
-      final byte[] data = new byte[4];
       for (int y = 0; y < 4; ++y) {
         final double fy = y;
         for (int x = 0; x < 4; ++x) {
           final double fx = x;
-          data[0] = (byte) (256.0 * (fx / 4.0));
-          data[1] = (byte) (256.0 * (fy / 4.0));
-          data[2] = (byte) 0xff;
-          data[3] = (byte) 0xff;
-          map.put(data);
+          c.put4b(
+            (byte) (256.0 * (fx / 4.0)),
+            (byte) (256.0 * (fy / 4.0)),
+            (byte) 0xff,
+            (byte) 0xff);
         }
       }
 
-      map.rewind();
+      assert c.hasNext() == false;
       this.gl.unmapPixelUnpackBuffer(this.texture.getBuffer());
     }
 
