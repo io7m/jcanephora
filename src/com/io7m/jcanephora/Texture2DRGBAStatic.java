2ce754440afd0be132666873faaf63e35201e462
Mark Raynsford <code@io7m.com>
2012-04-15 13:23:39 +0000

    Move image conversion to separate file

--- src/com/io7m/jcanephora/Texture2DRGBAStatic.java
+++ src/com/io7m/jcanephora/Texture2DRGBAStatic.java
@@ -1,7 +1,6 @@
 package com.io7m.jcanephora;
 
 import java.awt.image.BufferedImage;
-import java.awt.image.ColorConvertOp;
 import java.awt.image.DataBufferByte;
 import java.awt.image.WritableRaster;
 import java.nio.ByteBuffer;
@@ -23,44 +22,6 @@ import com.io7m.jaux.Constraints.ConstraintError;
 
 @Immutable public final class Texture2DRGBAStatic implements GLResource
 {
-  private static final @Nonnull BufferedImage convertImageToABGR(
-    final @Nonnull BufferedImage image)
-  {
-    BufferedImage converted = null;
-
-    final int width = image.getWidth();
-    final int height = image.getHeight();
-
-    switch (image.getType()) {
-      case BufferedImage.TYPE_4BYTE_ABGR:
-      case BufferedImage.TYPE_4BYTE_ABGR_PRE:
-      {
-        converted = image;
-        return converted;
-      }
-      case BufferedImage.TYPE_INT_ARGB_PRE:
-      case BufferedImage.TYPE_INT_ARGB:
-      case BufferedImage.TYPE_3BYTE_BGR:
-      case BufferedImage.TYPE_BYTE_GRAY:
-      case BufferedImage.TYPE_BYTE_BINARY:
-      case BufferedImage.TYPE_BYTE_INDEXED:
-      case BufferedImage.TYPE_USHORT_GRAY:
-      case BufferedImage.TYPE_USHORT_565_RGB:
-      case BufferedImage.TYPE_USHORT_555_RGB:
-      case BufferedImage.TYPE_CUSTOM:
-      case BufferedImage.TYPE_INT_RGB:
-      case BufferedImage.TYPE_INT_BGR:
-      default:
-      {
-        converted =
-          new BufferedImage(width, height, BufferedImage.TYPE_4BYTE_ABGR);
-        final ColorConvertOp op = new ColorConvertOp(null);
-        op.filter(image, converted);
-        return converted;
-      }
-    }
-  }
-
   public static Texture2DRGBAStatic loadImage(
     final @Nonnull String name,
     final @Nonnull BufferedImage image,
@@ -80,8 +41,7 @@ import com.io7m.jaux.Constraints.ConstraintError;
     Constraints.constrainNotNull(mag_filter, "Magnification filter");
     Constraints.constrainNotNull(min_filter, "Minification filter");
 
-    final BufferedImage converted =
-      Texture2DRGBAStatic.convertImageToABGR(image);
+    final BufferedImage converted = TextureUtils.convertImageToABGR(image);
     Texture2DRGBAStatic t = null;
     ByteBuffer map = null;
 
