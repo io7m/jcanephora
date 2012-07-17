22c9e1e0f1c315fb3b507cd4f82b1b186ff4c998
Mark Raynsford <code@io7m.com>
2012-07-17 17:08:00 +0000

    Change framebuffer API to statically disallow incomplete buffers

---
+++ io7m-jcanephora-core/src/main/java/com/io7m/jcanephora/GLFramebuffers.java
@@ -0,0 +1,116 @@
+package com.io7m.jcanephora;
+
+import javax.annotation.Nonnull;
+
+import com.io7m.jaux.Constraints.ConstraintError;
+
+/**
+ * Simplified interface to framebuffers.
+ */
+
+public interface GLFramebuffers
+{
+  /**
+   * Allocate a framebuffer and attach a set of buffers to it. Each element of
+   * <code>attachments</code> specifies the type of buffer (color, depth,
+   * etc), and possibly the attachment location (for color buffers).
+   * 
+   * @throws ConstraintError
+   *           Iff any of the following hold:
+   *           <ul>
+   *           <li><code>attachments == null</code></li>
+   *           <li><code>attachments.length == 0</code></li>
+   *           <li><code>∃n. attachments[n] == null</code></li>
+   *           <li>Multiple depth buffers are specified.</li>
+   *           <li>The number of specified color buffers is larger than an
+   *           implementation-defined maximum.</li>
+   *           </ul>
+   * @throws GLException
+   *           Iff an OpenGL exception occurs.
+   * @return A freshly allocated and initialized framebuffer.
+   */
+
+  @Nonnull Framebuffer framebufferAllocate(
+    final @Nonnull FramebufferAttachment[] attachments)
+    throws ConstraintError,
+      GLException;
+
+  /**
+   * Bind the given framebuffer as the current framebuffer.
+   * 
+   * @param buffer
+   *          The framebuffer.
+   * @throws ConstraintError
+   *           Iff <code>buffer == null</code>.
+   * @throws GLException
+   *           Iff an OpenGL error occurs.
+   */
+
+  void framebufferBind(
+    final @Nonnull Framebuffer buffer)
+    throws ConstraintError,
+      GLException;
+
+  /**
+   * Delete the framebuffer specified by <code>buffer</code>.
+   * 
+   * @param buffer
+   *          The buffer.
+   * @throws ConstraintError
+   *           Iff <code>buffer == null</code>.
+   * @throws GLException
+   *           Iff an OpenGL error occurs.
+   */
+
+  void framebufferDelete(
+    final @Nonnull Framebuffer buffer)
+    throws ConstraintError,
+      GLException;
+
+  /**
+   * Unbind the current framebuffer (if any).
+   * 
+   * @throws GLException
+   *           Iff an OpenGL error occurs.
+   */
+
+  void framebufferUnbind()
+    throws GLException;
+
+  /**
+   * Allocate a renderbuffer that will be used as storage for a combined depth
+   * and stencil buffer.
+   * 
+   * @return A freshly allocated renderbuffer.
+   * @throws ConstraintError
+   *           Iff any of the following hold:
+   *           <ul>
+   *           <li><code>1 <= width <= Integer.MAX_VALUE == false</code></li>
+   *           <li><code>1 <= height <= Integer.MAX_VALUE == false</code></li>
+   *           </ul>
+   * @throws GLException
+   *           Iff an OpenGL exception occurs.
+   */
+
+  @Nonnull RenderbufferD24S8 renderbufferD24S8Allocate(
+    final int width,
+    final int height)
+    throws ConstraintError,
+      GLException;
+
+  /**
+   * Delete the depth renderbuffer specified by <code>buffer</code>.
+   * 
+   * @param buffer
+   *          The buffer.
+   * @throws ConstraintError
+   *           Iff <code>buffer == null</code>.
+   * @throws GLException
+   *           Iff an OpenGL error occurs.
+   */
+
+  void renderbufferD24S8Delete(
+    final @Nonnull RenderbufferD24S8 buffer)
+    throws ConstraintError,
+      GLException;
+}
---
+++ io7m-jcanephora-core/src/test/java/com/io7m/jcanephora/contracts/FramebuffersContract.java
@@ -0,0 +1,296 @@
+package com.io7m.jcanephora.contracts;
+
+import junit.framework.Assert;
+
+import org.junit.Test;
+
+import com.io7m.jaux.Constraints.ConstraintError;
+import com.io7m.jcanephora.DepthFunction;
+import com.io7m.jcanephora.Framebuffer;
+import com.io7m.jcanephora.FramebufferAttachment;
+import com.io7m.jcanephora.FramebufferAttachment.ColorAttachment;
+import com.io7m.jcanephora.FramebufferAttachment.RenderbufferD24S8Attachment;
+import com.io7m.jcanephora.GLException;
+import com.io7m.jcanephora.GLInterface;
+import com.io7m.jcanephora.RenderbufferD24S8;
+import com.io7m.jcanephora.Texture2DRGBAStatic;
+import com.io7m.jcanephora.TextureFilter;
+import com.io7m.jcanephora.TextureWrap;
+
+public abstract class FramebuffersContract implements GLTestContract
+{
+  /**
+   * Attaching multiple depth/stencil buffer fails.
+   */
+
+  @Test(expected = ConstraintError.class) public
+    void
+    testAttachDepthMultiple()
+      throws GLException,
+        ConstraintError
+  {
+    final GLInterface g = this.getGL();
+    final RenderbufferD24S8 rb = g.renderbufferD24S8Allocate(128, 128);
+    final Texture2DRGBAStatic cb =
+      g.texture2DRGBAStaticAllocate(
+        "framebuffer",
+        128,
+        128,
+        TextureWrap.TEXTURE_WRAP_REPEAT,
+        TextureWrap.TEXTURE_WRAP_REPEAT,
+        TextureFilter.TEXTURE_FILTER_NEAREST,
+        TextureFilter.TEXTURE_FILTER_NEAREST);
+
+    g.framebufferAllocate(new FramebufferAttachment[] {
+      new ColorAttachment(cb, 0),
+      new RenderbufferD24S8Attachment(rb),
+      new RenderbufferD24S8Attachment(rb) });
+  }
+
+  /**
+   * A framebuffer with a D24S8 buffer attached has 24 bits of depth precision
+   * and 8 bits of stencil precision.
+   */
+
+  @Test public void testCheckDepthStencilBits()
+    throws GLException,
+      ConstraintError
+  {
+    final GLInterface g = this.getGL();
+
+    final RenderbufferD24S8 rb = g.renderbufferD24S8Allocate(128, 128);
+    final Texture2DRGBAStatic cb =
+      g.texture2DRGBAStaticAllocate(
+        "framebuffer",
+        128,
+        128,
+        TextureWrap.TEXTURE_WRAP_REPEAT,
+        TextureWrap.TEXTURE_WRAP_REPEAT,
+        TextureFilter.TEXTURE_FILTER_NEAREST,
+        TextureFilter.TEXTURE_FILTER_NEAREST);
+
+    final Framebuffer fb =
+      g.framebufferAllocate(new FramebufferAttachment[] {
+        new ColorAttachment(cb, 0),
+        new RenderbufferD24S8Attachment(rb) });
+
+    g.framebufferBind(fb);
+    Assert.assertTrue(g.depthBufferGetBits() == 24);
+    Assert.assertTrue(g.stencilBufferGetBits() == 8);
+    g.framebufferUnbind();
+  }
+
+  /**
+   * Enabling depth-testing on a framebuffer without a depth buffer fails.
+   */
+
+  @Test(expected = ConstraintError.class) public void testDepthEnableNone()
+    throws GLException,
+      ConstraintError
+  {
+    final GLInterface g = this.getGL();
+
+    try {
+      final Texture2DRGBAStatic cb =
+        g.texture2DRGBAStaticAllocate(
+          "framebuffer",
+          128,
+          128,
+          TextureWrap.TEXTURE_WRAP_REPEAT,
+          TextureWrap.TEXTURE_WRAP_REPEAT,
+          TextureFilter.TEXTURE_FILTER_NEAREST,
+          TextureFilter.TEXTURE_FILTER_NEAREST);
+
+      final Framebuffer fb =
+        g
+          .framebufferAllocate(new FramebufferAttachment[] { new FramebufferAttachment.ColorAttachment(
+            cb,
+            0) });
+      g.framebufferBind(fb);
+
+    } catch (final GLException e) {
+      Assert.fail(e.getMessage());
+    } catch (final ConstraintError e) {
+      Assert.fail(e.getMessage());
+    }
+
+    assert g != null;
+    g.depthBufferEnable(DepthFunction.DEPTH_LESS_THAN_OR_EQUAL);
+  }
+
+  /**
+   * Attaching a single color buffer to a framebuffer works.
+   */
+
+  @Test public void testFramebufferAttachColorOK()
+    throws ConstraintError,
+      GLException
+  {
+    final GLInterface gl = this.getGL();
+    final Texture2DRGBAStatic t =
+      gl.texture2DRGBAStaticAllocate(
+        "buffer",
+        128,
+        128,
+        TextureWrap.TEXTURE_WRAP_REPEAT,
+        TextureWrap.TEXTURE_WRAP_REPEAT,
+        TextureFilter.TEXTURE_FILTER_NEAREST,
+        TextureFilter.TEXTURE_FILTER_NEAREST);
+    final ColorAttachment fbc = new ColorAttachment(t, 0);
+    gl.framebufferAllocate(new FramebufferAttachment[] { fbc });
+  }
+
+  /**
+   * Attaching a single depth/stencil buffer to a framebuffer works.
+   */
+
+  @Test public void testFramebufferAttachDepthOK()
+    throws ConstraintError,
+      GLException
+  {
+    final GLInterface gl = this.getGL();
+    final RenderbufferD24S8 depth = gl.renderbufferD24S8Allocate(128, 128);
+    final RenderbufferD24S8Attachment fda =
+      new RenderbufferD24S8Attachment(depth);
+    gl.framebufferAllocate(new FramebufferAttachment[] { fda });
+  }
+
+  /**
+   * Attaching a two depth/stencil buffers to a framebuffer fails.
+   */
+
+  @Test(expected = ConstraintError.class) public
+    void
+    testFramebufferAttachDepthTwice()
+      throws ConstraintError,
+        GLException
+  {
+    final GLInterface gl = this.getGL();
+    final RenderbufferD24S8 depth = gl.renderbufferD24S8Allocate(128, 128);
+    final RenderbufferD24S8Attachment fda =
+      new RenderbufferD24S8Attachment(depth);
+    gl.framebufferAllocate(new FramebufferAttachment[] { fda, fda });
+  }
+
+  /**
+   * Attaching nothing to a framebuffer fails.
+   */
+
+  @Test(expected = ConstraintError.class) public
+    void
+    testFramebufferAttachListNone()
+      throws ConstraintError,
+        GLException
+  {
+    final GLInterface gl = this.getGL();
+    gl.framebufferAllocate(new FramebufferAttachment[] {});
+  }
+
+  /**
+   * Attaching a list with a null element fails.
+   */
+
+  @Test(expected = ConstraintError.class) public
+    void
+    testFramebufferAttachListNullElement()
+      throws ConstraintError,
+        GLException
+  {
+    final GLInterface gl = this.getGL();
+    gl.framebufferAllocate(new FramebufferAttachment[] { null });
+  }
+
+  /**
+   * Attaching null to a framebuffer fails.
+   */
+
+  @Test(expected = ConstraintError.class) public
+    void
+    testFramebufferAttachNull()
+      throws ConstraintError,
+        GLException
+  {
+    final GLInterface gl = this.getGL();
+    gl.framebufferAllocate(null);
+  }
+
+  /**
+   * Binding a deleted framebuffer fails.
+   */
+
+  @Test(expected = ConstraintError.class) public
+    void
+    testFramebufferBindDeleted()
+      throws ConstraintError,
+        GLException
+  {
+    final GLInterface gl = this.getGL();
+    final Texture2DRGBAStatic t =
+      gl.texture2DRGBAStaticAllocate(
+        "buffer",
+        128,
+        128,
+        TextureWrap.TEXTURE_WRAP_REPEAT,
+        TextureWrap.TEXTURE_WRAP_REPEAT,
+        TextureFilter.TEXTURE_FILTER_NEAREST,
+        TextureFilter.TEXTURE_FILTER_NEAREST);
+    final ColorAttachment fbc = new ColorAttachment(t, 0);
+    final Framebuffer fb =
+      gl.framebufferAllocate(new FramebufferAttachment[] { fbc });
+    fb.resourceDelete(gl);
+    gl.framebufferBind(fb);
+  }
+
+  /**
+   * Deleting a framebuffer works.
+   */
+
+  @Test public void testFramebufferDeleteOK()
+    throws ConstraintError,
+      GLException
+  {
+    final GLInterface gl = this.getGL();
+    final Texture2DRGBAStatic t =
+      gl.texture2DRGBAStaticAllocate(
+        "buffer",
+        128,
+        128,
+        TextureWrap.TEXTURE_WRAP_REPEAT,
+        TextureWrap.TEXTURE_WRAP_REPEAT,
+        TextureFilter.TEXTURE_FILTER_NEAREST,
+        TextureFilter.TEXTURE_FILTER_NEAREST);
+    final ColorAttachment fbc = new ColorAttachment(t, 0);
+    final Framebuffer fb =
+      gl.framebufferAllocate(new FramebufferAttachment[] { fbc });
+    fb.resourceDelete(gl);
+
+    Assert.assertTrue(fb.resourceIsDeleted());
+  }
+
+  /**
+   * Deleting a deleted framebuffer fails.
+   */
+
+  @Test(expected = ConstraintError.class) public
+    void
+    testFramebufferDeleteTwice()
+      throws ConstraintError,
+        GLException
+  {
+    final GLInterface gl = this.getGL();
+    final Texture2DRGBAStatic t =
+      gl.texture2DRGBAStaticAllocate(
+        "buffer",
+        128,
+        128,
+        TextureWrap.TEXTURE_WRAP_REPEAT,
+        TextureWrap.TEXTURE_WRAP_REPEAT,
+        TextureFilter.TEXTURE_FILTER_NEAREST,
+        TextureFilter.TEXTURE_FILTER_NEAREST);
+    final ColorAttachment fbc = new ColorAttachment(t, 0);
+    final Framebuffer fb =
+      gl.framebufferAllocate(new FramebufferAttachment[] { fbc });
+    fb.resourceDelete(gl);
+    fb.resourceDelete(gl);
+  }
+}
