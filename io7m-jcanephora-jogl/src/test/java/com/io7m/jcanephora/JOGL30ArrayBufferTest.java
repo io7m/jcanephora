e97f98348cf117ace019d3f76323e6fd78e372f4
Mark Raynsford <code@io7m.com>
2012-07-19 11:41:48 +0000

    Add array vertex attribute tests

--- io7m-jcanephora-jogl/src/test/java/com/io7m/jcanephora/JOGL30ArrayBufferTest.java
+++ io7m-jcanephora-jogl/src/test/java/com/io7m/jcanephora/JOGL30ArrayBufferTest.java
@@ -3,9 +3,15 @@ package com.io7m.jcanephora;
 import com.io7m.jaux.Constraints.ConstraintError;
 import com.io7m.jcanephora.contracts.ArrayBufferContract;
 import com.io7m.jlog.Log;
+import com.io7m.jvvfs.FilesystemAPI;
 
 public final class JOGL30ArrayBufferTest extends ArrayBufferContract
 {
+  @Override public FilesystemAPI getFS()
+  {
+    return JOGL30TestFilesystem.getFS();
+  }
+
   @Override public GLInterface getGL()
     throws GLException,
       ConstraintError
