7005406c57ce2c2f30ce8e799424be65d216ee36
Mark Raynsford <code@io7m.com>
2012-07-20 18:02:33 +0000

    Improve javadoc, documentation, and import new structural

---
+++ io7m-jcanephora-core/src/main/java/com/io7m/jcanephora/Primitives.java
@@ -0,0 +1,14 @@
+package com.io7m.jcanephora;
+
+/**
+ * Primitive type selection.
+ */
+
+public enum Primitives
+{
+  PRIMITIVE_LINES,
+  PRIMITIVE_LINE_LOOP,
+  PRIMITIVE_POINTS,
+  PRIMITIVE_TRIANGLES,
+  PRIMITIVE_TRIANGLE_STRIP,
+}
