dd0d255aa70ec01ecf6ef6b30178dd0f4a2e8e57
Mark Raynsford <code@io7m.com>
2012-07-20 18:02:33 +0000

    Improve javadoc, documentation, and import new structural

---
+++ io7m-jcanephora-core/src/main/java/com/io7m/jcanephora/UsableProgram.java
@@ -0,0 +1,46 @@
+package com.io7m.jcanephora;
+
+import javax.annotation.Nonnull;
+
+import com.io7m.jaux.Constraints.ConstraintError;
+
+/**
+ * A usable shading language program.
+ */
+
+public interface UsableProgram
+{
+  /**
+   * Make the program active.
+   * 
+   * @param gl
+   *          An OpenGL interface.
+   * @throws ConstraintError
+   *           Iff <code>gl == null</code> or one of the constraints for
+   *           {@link GLInterface#programActivate(ProgramReference)} does not
+   *           hold.
+   * @throws GLException
+   *           Iff an OpenGL error occurs.
+   */
+
+  void activate(
+    final @Nonnull GLInterface gl)
+    throws ConstraintError,
+      GLException;
+
+  /**
+   * Deactivate the current program. If the current program is not active, the
+   * function does nothing.
+   * 
+   * @param gl
+   * @throws ConstraintError
+   *           Iff <code>gl == null</code>.
+   * @throws GLException
+   *           Iff an OpenGL error occurs.
+   */
+
+  void deactivate(
+    final @Nonnull GLInterface gl)
+    throws ConstraintError,
+      GLException;
+}
