cd06b841f628d65e75d17a07f28cbaa782a9acee
Mark Raynsford <code@io7m.com>
2012-12-29 19:25:12 +0000

    Remove face selection for polygon modes, affects [6b3fb24570]

--- io7m-jcanephora-core/src/main/java/com/io7m/jcanephora/GLPolygonModes.java
+++ io7m-jcanephora-core/src/main/java/com/io7m/jcanephora/GLPolygonModes.java
@@ -27,38 +27,26 @@ import com.io7m.jaux.Constraints.ConstraintError;
 public interface GLPolygonModes
 {
   /**
-   * Return the <code>PolygonMode</code> used for back-facing polygons.
+   * Return the <code>PolygonMode</code> used for polygons.
    */
 
-  @Nonnull PolygonMode polygonGetModeBack()
+  @Nonnull PolygonMode polygonGetMode()
     throws ConstraintError,
       GLException;
 
   /**
-   * Return the <code>PolygonMode</code> used for front-facing polygons.
-   */
-
-  @Nonnull PolygonMode polygonGetModeFront()
-    throws ConstraintError,
-      GLException;
-
-  /**
-   * Set the polygon rasterization mode for the polygons specified by
-   * <code>faces</code> to <code>mode</code>. The OpenGL default is
-   * <code>POLYGON_FILL</code> for both front and back faces.
+   * Set the polygon rasterization mode for the polygons to <code>mode</code>.
+   * The OpenGL default is <code>POLYGON_FILL</code>.
    * 
-   * @param faces
-   *          The selected faces.
    * @param mode
    *          The rasterization mode.
    * @throws ConstraintError
-   *           Iff <code>faces == null | mode == null</code>.
+   *           Iff <code>mode == null</code>.
    * @throws GLException
    *           Iff an OpenGL error occurs.
    */
 
   void polygonSetMode(
-    final @Nonnull FaceSelection faces,
     final @Nonnull PolygonMode mode)
     throws ConstraintError,
       GLException;
