58655851f0f190f219d763d305aa46323febe0a2
Mark Raynsford <code@io7m.com>
2013-01-02 23:25:44 +0000

    Add missing TestContext file

---
+++ io7m-jcanephora-tests/src/main/java/com/io7m/jcanephora/TestContext.java
@@ -0,0 +1,47 @@
+package com.io7m.jcanephora;
+
+import javax.annotation.Nonnull;
+
+import com.io7m.jlog.Log;
+import com.io7m.jvvfs.FilesystemAPI;
+import com.io7m.jvvfs.PathVirtual;
+
+public final class TestContext
+{
+  private final @Nonnull FilesystemAPI    fs;
+  private final @Nonnull GLImplementation gi;
+  private final @Nonnull Log              log;
+  private final @Nonnull PathVirtual      shader_path;
+
+  public TestContext(
+    final @Nonnull FilesystemAPI fs,
+    final @Nonnull GLImplementation gi,
+    final @Nonnull Log log,
+    final @Nonnull PathVirtual shader_path)
+  {
+    this.fs = fs;
+    this.gi = gi;
+    this.log = log;
+    this.shader_path = shader_path;
+  }
+
+  public GLImplementation getGLImplementation()
+  {
+    return this.gi;
+  }
+
+  public Log getLog()
+  {
+    return this.log;
+  }
+
+  public PathVirtual getShaderPath()
+  {
+    return this.shader_path;
+  }
+
+  public FilesystemAPI getFilesystem()
+  {
+    return this.fs;
+  }
+}
