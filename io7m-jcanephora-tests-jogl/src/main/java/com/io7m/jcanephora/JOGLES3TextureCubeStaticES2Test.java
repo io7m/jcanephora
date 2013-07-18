ad6bb73f3d93d9112b24ba83c46d884e9647927e
Mark Raynsford <code@io7m.com>
2013-07-18 22:09:30 +0000

    Add missing test cases

---
+++ io7m-jcanephora-tests-jogl/src/main/java/com/io7m/jcanephora/JOGLES3TextureCubeStaticES2Test.java
@@ -0,0 +1,55 @@
+/*
+ * Copyright © 2013 <code@io7m.com> http://io7m.com
+ * 
+ * Permission to use, copy, modify, and/or distribute this software for any
+ * purpose with or without fee is hereby granted, provided that the above
+ * copyright notice and this permission notice appear in all copies.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY
+ * SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF OR
+ * IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.
+ */
+package com.io7m.jcanephora;
+
+import javax.annotation.Nonnull;
+
+import com.io7m.jaux.Constraints.ConstraintError;
+import com.io7m.jaux.functional.Option.Some;
+import com.io7m.jcanephora.contracts.gles2.TextureCubeStaticES2Contract;
+
+public final class JOGLES3TextureCubeStaticES2Test extends
+  TextureCubeStaticES2Contract
+{
+  @Override public GLTexturesCubeStaticCommon getGLTextureCubeStaticCommon(
+    final TestContext tc)
+  {
+    final Some<GLInterfaceGLES3> some =
+      (Some<GLInterfaceGLES3>) tc.getGLImplementation().getGLES3();
+    return some.value;
+  }
+
+  @Override public GLTextureUnits getGLTextureUnits(
+    final TestContext tc)
+  {
+    final Some<GLInterfaceGLES3> some =
+      (Some<GLInterfaceGLES3>) tc.getGLImplementation().getGLES3();
+    return some.value;
+  }
+
+  @Override public boolean isGLSupported()
+  {
+    return JOGLTestContext.isOpenGLES3Supported();
+  }
+
+  @Override public @Nonnull TestContext newTestContext()
+    throws GLException,
+      GLUnsupportedException,
+      ConstraintError
+  {
+    return JOGLTestContext.makeContextWithOpenGL_ES3();
+  }
+}
