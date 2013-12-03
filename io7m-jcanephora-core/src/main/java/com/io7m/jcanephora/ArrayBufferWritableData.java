ad14b44049f7b32e182b8869085420b3d05ac7c0
Mark Raynsford <code@io7m.com>
2013-12-03 16:20:35 +0000

    Enforce non-null ranges for writable data

--- io7m-jcanephora-core/src/main/java/com/io7m/jcanephora/ArrayBufferWritableData.java
+++ io7m-jcanephora-core/src/main/java/com/io7m/jcanephora/ArrayBufferWritableData.java
@@ -83,7 +83,7 @@ public final class ArrayBufferWritableData
     throws ConstraintError
   {
     this.buffer = Constraints.constrainNotNull(buffer, "Array buffer");
-    this.range = range;
+    this.range = Constraints.constrainNotNull(range, "Range");
 
     Constraints.constrainArbitrary(
       range.isIncludedIn(buffer.getRange()),
