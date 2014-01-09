83f1d09a4f8bbd28fdd8ed1c2d143fee54e3ecc7
Mark Raynsford <code@io7m.com>
2014-01-09 16:32:36 +0000

    Add getBytePerPixel to RenderbufferType. Fixes [365789b173].

--- io7m-jcanephora-core/src/main/java/com/io7m/jcanephora/RenderbufferType.java
+++ io7m-jcanephora-core/src/main/java/com/io7m/jcanephora/RenderbufferType.java
@@ -33,56 +33,57 @@ public enum RenderbufferType
    * Four-channel RGBA, 5 bits R, 6 bits G, 5 bits B, two bytes per pixel.
    */
 
-  RENDERBUFFER_COLOR_RGB_565,
+  RENDERBUFFER_COLOR_RGB_565(2),
 
   /**
    * Three-channel RGB, 8 bits per channel, three bytes per pixel.
    */
 
-  RENDERBUFFER_COLOR_RGB_888,
+  RENDERBUFFER_COLOR_RGB_888(3),
 
   /**
    * Four-channel RGBA, 4 bits per channel, two bytes per pixel.
    */
 
-  RENDERBUFFER_COLOR_RGBA_4444,
+  RENDERBUFFER_COLOR_RGBA_4444(2),
 
   /**
    * Four-channel RGBA, 5 bits per (R, G, B) channels, 1 bit alpha, two bytes
    * per pixel.
    */
 
-  RENDERBUFFER_COLOR_RGBA_5551,
+  RENDERBUFFER_COLOR_RGBA_5551(2),
 
   /**
    * Four-channel RGBA, 8 bits per channel, four bytes per pixel.
    */
 
-  RENDERBUFFER_COLOR_RGBA_8888,
+  RENDERBUFFER_COLOR_RGBA_8888(4),
 
   /**
    * 16 bit depth buffer
    */
 
-  RENDERBUFFER_DEPTH_16,
+  RENDERBUFFER_DEPTH_16(2),
 
   /**
-   * 24 bit depth buffer
+   * 24 bit depth buffer (implementation dependent: most likely four bytes per
+   * pixel).
    */
 
-  RENDERBUFFER_DEPTH_24,
+  RENDERBUFFER_DEPTH_24(4),
 
   /**
    * Packed 24 bit depth buffer, 8 bit stencil buffer
    */
 
-  RENDERBUFFER_DEPTH_24_STENCIL_8,
+  RENDERBUFFER_DEPTH_24_STENCIL_8(4),
 
   /**
    * 8 bit stencil buffer
    */
 
-  RENDERBUFFER_STENCIL_8;
+  RENDERBUFFER_STENCIL_8(1);
 
   /**
    * The subset of renderbuffer types supported by both OpenGL 3.* and ES 3.*.
@@ -139,6 +140,23 @@ public enum RenderbufferType
     s0.addAll(e0);
     s0.addAll(e1);
     return s0;
+  }
+
+  private int bytes_per_pixel;
+
+  private RenderbufferType(
+    final int size)
+  {
+    this.bytes_per_pixel = size;
+  }
+
+  /**
+   * Retrieve the number of bytes per pixel that this texture type requires.
+   */
+
+  public int getBytesPerPixel()
+  {
+    return this.bytes_per_pixel;
   }
 
   /**
