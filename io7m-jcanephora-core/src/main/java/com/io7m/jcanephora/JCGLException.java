ac981eba9750e20da949ab3d02b802f45988f1af
Mark Raynsford <code@io7m.com>
2014-09-14 16:53:48 +0000

    JCGLException is now unchecked. Implements [78a01d0d8d].

--- io7m-jcanephora-core/src/main/java/com/io7m/jcanephora/JCGLException.java
+++ io7m-jcanephora-core/src/main/java/com/io7m/jcanephora/JCGLException.java
@@ -1,10 +1,10 @@
 /*
  * Copyright © 2014 <code@io7m.com> http://io7m.com
- * 
+ *
  * Permission to use, copy, modify, and/or distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
  * copyright notice and this permission notice appear in all copies.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
  * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
  * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY
@@ -20,7 +20,7 @@ package com.io7m.jcanephora;
  * The root exception type of all exceptions raised by <tt>jcanephora</tt>.
  */
 
-public abstract class JCGLException extends Exception
+public abstract class JCGLException extends RuntimeException
 {
   private static final long serialVersionUID;
 
@@ -30,7 +30,7 @@ public abstract class JCGLException extends Exception
 
   /**
    * Construct an error with the given message.
-   * 
+   *
    * @param message
    *          The message.
    */
@@ -43,7 +43,7 @@ public abstract class JCGLException extends Exception
 
   /**
    * Construct an error with the given message and cause.
-   * 
+   *
    * @param message
    *          The message.
    * @param e
@@ -59,7 +59,7 @@ public abstract class JCGLException extends Exception
 
   /**
    * Construct an error with the given cause.
-   * 
+   *
    * @param e
    *          The cause.
    */
