946aea680a89c6af4dc223d36bc9c7b1fee7fabc
Mark Raynsford <code@io7m.com>
2014-09-30 19:20:15 +0000

    Fix unintentionally unreachable code

--- io7m-jcanephora-jogl/src/main/java/com/io7m/jcanephora/jogl/JCGLInterfaceGL2_JOGL_GL21.java
+++ io7m-jcanephora-jogl/src/main/java/com/io7m/jcanephora/jogl/JCGLInterfaceGL2_JOGL_GL21.java
@@ -1,10 +1,10 @@
 /*
  * Copyright © 2014 <code@io7m.com> http://io7m.com
- *
+ * 
  * Permission to use, copy, modify, and/or distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
  * copyright notice and this permission notice appear in all copies.
- *
+ * 
  * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES
  * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF
  * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR ANY
@@ -136,7 +136,6 @@ import com.io7m.jtensors.VectorReadable4IType;
 
 final class JCGLInterfaceGL2_JOGL_GL21 implements JCGLInterfaceGL2Type
 {
-
   private static GL2 makeCachedGL2(
     final boolean in_debugging,
     final OptionType<PrintStream> in_tracing,
